be981d09c15cb1cb1ce520f0aa8b9e46809befb0
Wen Ting <2289723167@qq.com>
2019-05-28 10:45:13 +0800

    修改了main.js和测试用例

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    let price = 0;
+    if (inputs.distance <= 2){
+        price = 6;
+    }
+    else if (inputs.distance <= 8 && inputs.distance > 2){
+        price = 6 + 0.8 * (inputs.distance - 2);
+    }
+    else if (inputs.distance > 8){
+        price = 10.8 + 1.2 * (inputs.distance - 8)
+    }
+    else throw Error
+    if (inputs.parkTime > 0){
+        price += inputs.parkTime * 0.25
+    }
+    return Math.round(price);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,7 +1,7 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    it('within 2km and parking' , function() {
+    it('within 2km and no parking' , function() {
         let inputs = {distance:1,parkTime:5};
         let summary = main(inputs);
         let expected = 7;
@@ -13,16 +13,34 @@ describe('taxi fee', function () {
         let expected = 13;
         expect(summary).toEqual(expected);
     });
-    it('more than 8km and parking' , function() {
-        let inputs = {distance:10,parkTime:20};
-        let summary = main(inputs);
-        let expected = 18;
-        expect(summary).toEqual(expected);
-    });
     it('more than 8km and no parking' , function() {
         let inputs = {distance:15,parkTime:0};
         let summary = main(inputs);
         let expected = 19;
+        expect(summary).toEqual(expected);
+    });
+    it('在2KM以内，无停车' , function() {
+        let inputs = {distance:1,parkTime:0};
+        let summary = main(inputs);
+        let expected = 6;
+        expect(summary).toEqual(expected);
+    });
+    it('在2km和8km之间，无停车' , function() {
+        let inputs = {distance:7,parkTime:0};
+        let summary = main(inputs);
+        let expected = 10;
+        expect(summary).toEqual(expected);
+    });
+    it('超过8km，无停车' , function() {
+        let inputs = {distance:10,parkTime:0};
+        let summary = main(inputs);
+        let expected = 13;
+        expect(summary).toEqual(expected);
+    });
+    it('超过8km，有停车' , function() {
+        let inputs = {distance:15,parkTime:20};
+        let summary = main(inputs);
+        let expected = 24;
         expect(summary).toEqual(expected);
     });
 });
